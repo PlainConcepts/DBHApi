bc8bbe641bca1a9df05da30e6ca9b95eaa72ee87
borja andrés <borya09@hotmail.com>
2014-09-23 14:05:41 +0200

    trying to push master on release branch when build is ok

--- deploy-release.sh
+++ deploy-release.sh
@@ -10,12 +10,11 @@ if [ ! -d "$SOURCE_DIR" ]; then
   exit 1
 fi
 
-REPO=$(git config remote.origin.url)
-
 if [ -n "$TRAVIS_BUILD_ID" ]; then
 
   echo TARGET_BRANCH: $TARGET_BRANCH
   echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+  echo REPO: $REPO
 
   if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
@@ -28,26 +27,17 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
   fi
 fi
 
-git config --global user.email ${GIT_EMAIL}
-git config --global user.name ${GIT_USER}
 
-echo config --global user.email
-echo config --global user.name
 
 REPO_NAME=$(basename $REPO)
 TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
-
-echo '0'
 REV=$(git rev-parse HEAD)
 
+git config --global user.email ${GIT_EMAIL}
+git config --global user.name ${GIT_USER}
 git remote set-url origin $REPO
-
-echo '0.2'
-
 git clone --branch $TARGET_BRANCH $REPO ${TARGET_DIR}
 
-
-echo '1'
 rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
 cd $TARGET_DIR
 git add -A .
