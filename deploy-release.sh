7bdb1650c92d22fc958dee3e561d755893ea3f1e
borja andrés <borya09@hotmail.com>
2014-09-23 12:57:12 +0200

    trying to push master on release branch when build is ok

--- deploy-release.sh
+++ deploy-release.sh
@@ -12,26 +12,9 @@ fi
 REPO=$(git config remote.origin.url)
 
 if [ -n "$TRAVIS_BUILD_ID" ]; then
-  # When running on Travis we need to use SSH to deploy to GitHub
-  #
-  # The following converts the repo URL to an SSH location,
-  # decrypts the SSH key and sets up the Git config with
-  # the correct user name and email (globally as this is a
-  # temporary travis environment)
-  #
-  # Set the following environment variables in the travis configuration (.travis.yml)
-  #
-  #   TARGET_BRANCH    - The branch where will be pushed the content of the deploy branch
-  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-  #   GIT_NAME         - The Git user name
-  #   GIT_EMAIL        - The Git user email
-  #
+
   echo TARGET_BRANCH: $TARGET_BRANCH
   echo DEPLOY_BRANCH: $DEPLOY_BRANCH
-  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
-  echo GIT_NAME: $GIT_NAME
-  echo GIT_EMAIL: $GIT_EMAIL
 
   if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
@@ -57,7 +40,7 @@ echo '0'
 REV=$(git rev-parse HEAD)
 
 git remote set-url origin https://${GIT_TOKEN}@github.com/PlainConcepts/DBHApi.git
-git clone --branch ${TARGET_BRANCH} https://${GIT_TOKEN}@github.com/PlainConcepts/DBHApi.git ${TARGET_DIR}
+git clone https://${GIT_TOKEN}@github.com/PlainConcepts/DBHApi.git ${TARGET_DIR}
 
 
 echo '1'
